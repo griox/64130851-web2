aee24a7be77217bd8aa7729de71703dd5e744614
Huy Ngô <huyngo@Huys-MacBook-Air.local>
2025-03-25 09:15:04 +0700

    first commit

---
+++ GiuaKi/src/main/java/huyquangngo/main/giuaki/GiuaKiApplication.java
@@ -0,0 +1,13 @@
+package huyquangngo.main.giuaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiuaKiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiuaKiApplication.class, args);
+    }
+
+}
---
+++ GiuaKi/src/test/java/huyquangngo/main/giuaki/GiuaKiApplicationTests.java
@@ -0,0 +1,13 @@
+package huyquangngo.main.giuaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiuaKiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
