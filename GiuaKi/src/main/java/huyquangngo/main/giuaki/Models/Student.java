f17c7a690dc000d1f973305f0435e193846ff101
Huy Ngô <huyngo@Huys-MacBook-Air.local>
2025-03-25 10:40:06 +0700

    hien thi trang about va list

---
+++ GiuaKi/src/main/java/huyquangngo/main/giuaki/Models/Student.java
@@ -0,0 +1,11 @@
+package huyquangngo.main.giuaki.Models;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Student{
+    private String Mssv;
+    private String hoTen;
+    private boolean gioiTinh;
+    private float diemTB;
+}
